dbf571d6021696a85e7764ef3b34f89b61426b88
Willem Evenwel <willem@stablesolutions.co.za>
2021-06-17 15:58:53 +0200

    Ignore file not working nicely yet Stupid logger doesnt log yet.

--- src/main/java/pvt/willemevenwel/span/Main.java
+++ src/main/java/pvt/willemevenwel/span/Main.java
@@ -1,10 +1,15 @@
 package pvt.willemevenwel.span;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+    final static Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
 
-        System.out.println("Willem says Hello World.");
+        logger.info("Willem says Hello World.");
 
     }
 
